2d76d8c3be3d2a0318e6b8aed13ca7d9e6027fb9
IM23a-tutkuno <tutkuno@bzz.ch>
2025-12-02 10:54:33 +0100

    test

--- src/main/java/ch/bzz/rag/ApplicationMain.java
+++ src/main/java/ch/bzz/rag/ApplicationMain.java
@@ -2,11 +2,21 @@ package ch.bzz.rag;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.MalformedURLException;
+import ch.bzz.rag.service.WikiPageDownloaderService;
 
 @Slf4j
 @SpringBootApplication
 public class ApplicationMain {
     public static void main(String[] args) {
         log.info("Application has been started");
+        WikiPageDownloaderService downloader = new WikiPageDownloaderService();
+        try {
+            downloader.init("https://wiki.bzz.ch");
+            String content = downloader.downloadPage("de:modul:ffit:3-jahr:java:learningunits:lu01:aufgaben:branching");
+            log.info("content: '{}'", content);
+        } catch (MalformedURLException e) {
+            log.error("Error using url {}", e.getMessage(), e);
+        }
     }
 }
